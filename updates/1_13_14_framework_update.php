4d8317f9d45f0889ff4ea5baa9e123e66fa7935d
Matt <matt@mjman.net>
2018-10-11 12:46:02 +1100

    fix 1.13.14 file copy

--- updates/1_13_14_framework_update.php
+++ updates/1_13_14_framework_update.php
@@ -1,7 +1,7 @@
 <?php
 $file_name = '_form_field_dropdown.htm';
-$current_file = PATH_APP.'/phproad/modules/db/behaviors/partials/'.$file_name;
-$replacement_file = PATH_APP.'/modules/core/updates/framework/phproad/modules/db/behaviors/partials/'.$file_name;
+$current_file = PATH_APP.'/phproad/modules/db/behaviors/db_formbehavior/partials/'.$file_name;
+$replacement_file = PATH_APP.'/modules/core/updates/framework/phproad/modules/db/behaviors/db_formbehavior/partials/'.$file_name;
 if(!copy($replacement_file, $current_file)){
 	throw new Phpr_ApplicationException('Could not copy '.$file_name.' to '.$current_file.' check write permissions for PHP');
 }
