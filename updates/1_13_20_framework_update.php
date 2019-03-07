a40e403b91546d289f86bbfac30e3c251c9518cd
Matt <matt@mjman.net>
2019-03-07 12:39:29 +1100

    Minor framework update for Db_ModuleParameters

---
+++ updates/1_13_20_framework_update.php
@@ -0,0 +1,7 @@
+<?php
+$file_name = 'db_moduleparameters.php';
+$current_file = PATH_APP.'/phproad/modules/db/classes/'.$file_name;
+$replacement_file = PATH_APP.'/modules/core/updates/framework/phproad/modules/db/classes/'.$file_name;
+if(!copy($replacement_file, $current_file)) {
+	throw new Phpr_ApplicationException( 'Could not copy ' . $file_name . ' to ' . $current_file . ' check write permissions for PHP' );
+}
