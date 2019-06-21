72f34e4c584353f4eeaab808d6d420b7f09ce92d
Matt <matt@mjman.net>
2019-06-21 12:51:17 +1000

    Minor framework update adds get_query_string() function to phpr_request

---
+++ updates/1_13_23_framework_update.php
@@ -0,0 +1,7 @@
+<?php
+$file_name = 'phpr_request.php';
+$current_file = PATH_APP.'/phproad/modules/phpr/classes/'.$file_name;
+$replacement_file = PATH_APP.'/modules/core/updates/framework/phproad/modules/phpr/classes/'.$file_name;
+if(!copy($replacement_file, $current_file)) {
+	throw new Phpr_ApplicationException( 'Could not copy ' . $file_name . ' to ' . $current_file . ' check write permissions for PHP' );
+}
