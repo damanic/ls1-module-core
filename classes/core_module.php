96fbb6010b015f511d62ee7452bb4e8048f41243
Matt <matt@mjman.net>
2019-10-25 15:25:33 +1100

    silence unlink

--- classes/core_module.php
+++ classes/core_module.php
@@ -35,7 +35,7 @@
 			$framework_folder = PATH_APP.'/phproad/';
 			if (file_exists($framework_update)){
 				Core_ZipHelper::unzip($framework_folder, $framework_update, $update_file_permissions = true);
-				unlink($framework_update);
+				@unlink($framework_update);
 			}
 		}
 		
